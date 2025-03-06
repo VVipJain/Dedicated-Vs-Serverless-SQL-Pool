5879da0317bf9f85d5868cb02e09f73240502ee7
Vipul Jain <107971515+VVipJain@users.noreply.github.com>
2025-03-06 11:21:57 +0530

    Delete SQL script 1 (1).sql

--- SQL script 1 (1).sql
+++
@@ -1,9 +0,0 @@
--- This is auto-generated code
-SELECT
-    TOP 100 *
-FROM
-    OPENROWSET(
-        BULK 'https://vipulsynapsestorage.dfs.core.windows.net/raw/revenue/revenue_star.csv',
-        FORMAT = 'CSV',
-        PARSER_VERSION = '2.0'
-    ) AS [result]
